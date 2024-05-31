b6cc926c788074c48b327cb8f7caf6d0b7800cb8
rmarci1 <rigbimarci@gmail.com>
2024-05-31 15:14:30 +0200

    tesztelések megcsinálása

---
+++ TestJaratKezeloProject/Jaratkezelo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJaratKezeloProject
+{
+    public class JaratKezelo
+    {
+        private class Jarat
+        {
+            public string JaratSzam { get; }
+            public string RepterHonnan { get; }
+            public string RepterHova { get; }
+            public DateTime Indulas { get; }
+            public int Keses { get; set; }
+
+            public Jarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+            {
+                JaratSzam = jaratSzam;
+                RepterHonnan = repterHonnan;
+                RepterHova = repterHova;
+                Indulas = indulas;
+                Keses = 0;
+            }
+        }
+
+        private readonly Dictionary<string, Jarat> jaratok = new Dictionary<string, Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            if (jaratok.ContainsKey(jaratSzam))
+                throw new ArgumentException("A járatszámnak egyedinek kell lennie!");
+
+            var jarat = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok[jaratSzam] = jarat;
+        }
+
+        public void Keses(string jaratSzam, int keses)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+                throw new ArgumentException("Nem létező járat!");
+
+            var jarat = jaratok[jaratSzam];
+            jarat.Keses += keses;
+
+            if (jarat.Keses < 0)
+            {
+                jarat.Keses -= keses; // Visszaállítjuk az előző állapotot
+                throw new ArgumentException("A késés nem lehet negatív!");
+            }
+        }
+
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+                throw new ArgumentException("Nem létező járat!");
+
+            var jarat = jaratok[jaratSzam];
+            return jarat.Indulas.AddMinutes(jarat.Keses);
+        }
+
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            var result = new List<string>();
+            foreach (var jarat in jaratok.Values)
+            {
+                if (jarat.RepterHonnan == repter)
+                    result.Add(jarat.JaratSzam);
+            }
+            return result;
+        }
+    }
+}
--- TestJaratKezeloProject/UnitTest1.cs
+++ TestJaratKezeloProject/UnitTest1.cs
@@ -1,16 +1,80 @@
+using NUnit.Framework;
+using System;
+
 namespace TestJaratKezeloProject
 {
-    public class Tests
+    [TestFixture]
+    public class JaratKezeloTests
     {
+        private JaratKezelo jaratKezelo;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            jaratKezelo = new JaratKezelo();
         }
 
         [Test]
-        public void Test1()
+        public void UjJarat_ShouldAddNewJarat()
         {
-            Assert.Pass();
+            jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+            var indulasiIdo = jaratKezelo.MikorIndul("A21");
+
+            Assert.AreEqual(indulasiIdo.Date, DateTime.Now.Date);
+        }
+
+        [Test]
+        public void UjJarat_DuplicateJaratSzam_ShouldThrowException()
+        {
+            jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+            });
+        }
+
+        [Test]
+        public void Keses_ShouldUpdateKeses()
+        {
+            jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+            jaratKezelo.Keses("A21", 15);
+            var indulasiIdo = jaratKezelo.MikorIndul("A21");
+
+            Assert.AreEqual(indulasiIdo, DateTime.Now.AddMinutes(15));
+        }
+
+        [Test]
+        public void Keses_NegativeResult_ShouldThrowException()
+        {
+            jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                jaratKezelo.Keses("A21", -10);
+            });
+        }
+
+        [Test]
+        public void MikorIndul_NonExistentJarat_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                jaratKezelo.MikorIndul("NonExistent");
+            });
+        }
+
+        [Test]
+        public void JaratokRepuloterrol_ShouldReturnCorrectJaratok()
+        {
+            jaratKezelo.UjJarat("A21", "ame", "fol", DateTime.Now);
+            jaratKezelo.UjJarat("A24", "ame", "fil", DateTime.Now);
+
+            var result = jaratKezelo.JaratokRepuloterrol("ame");
+
+            Assert.AreEqual(2, result.Count);
+            Assert.Contains("A21", result);
+            Assert.Contains("22", result);
         }
     }
 }
